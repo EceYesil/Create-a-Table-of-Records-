b1777fecece0ca41b65607379f9507a8bbbb40b1
“Ece <eceyel@yahoo.comgit config --global user.email eceyel@yahoo.com>
2024-11-04 23:59:30 +0300

    Datetime replaced with decimal.

--- TableOfRecords/TableOfRecordsCreator.cs
+++ TableOfRecords/TableOfRecordsCreator.cs
@@ -26,80 +26,102 @@ namespace TableOfRecords
         /// <exception cref="ArgumentException">Throw if <paramref name="collection"/> is empty.</exception>
         public static void WriteTable<T>(ICollection<T>? collection, TextWriter? writer)
         {
-            if (collection != null)
-            {
-                if (writer != null)
-                {
-                    if (collection.Count == 0)
-                    {
-                        throw new ArgumentException("Collection cannot be empty.", nameof(collection));
-                    }
-
-                    var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                                              .Where(p => p.PropertyType.IsPrimitive || p.PropertyType == typeof(string) || p.PropertyType == typeof(DateTime))
-                                              .ToArray();
-
-                    if (properties.Length == 0)
-                    {
-                        return;
-                    }
-
-                    var columnWidths = new int[properties.Length];
-
-                    foreach (var item in collection)
-                    {
-                        for (int i = 0; i < properties.Length; i++)
-                        {
-                            var value = properties[i].GetValue(item)?.ToString() ?? string.Empty;
-                            columnWidths[i] = Math.Max(columnWidths[i], value.Length);
-                        }
-                    }
-
-                    // Write header
-                    for (int i = 0; i < properties.Length; i++)
-                    {
-                        var header = properties[i].Name;
-                        columnWidths[i] = Math.Max(columnWidths[i], header.Length);
-                        writer.Write(header.PadRight(columnWidths[i] + 2));
-                    }
-
-                    writer.WriteLine();
-
-                    // Write separator
-                    for (int i = 0; i < properties.Length; i++)
-                    {
-                        writer.Write(new string('-', columnWidths[i] + 2));
-                    }
-
-                    writer.WriteLine();
-
-                    // Write rows
-                    foreach (var item in collection)
-                    {
-                        for (int i = 0; i < properties.Length; i++)
-                        {
-                            var value = properties[i].GetValue(item)?.ToString() ?? string.Empty;
-                            if (properties[i].PropertyType == typeof(int) || properties[i].PropertyType == typeof(double) || properties[i].PropertyType == typeof(float) || properties[i].PropertyType == typeof(decimal) || properties[i].PropertyType == typeof(DateTime))
-                            {
-                                writer.Write(value.PadLeft(columnWidths[i] + 2));
-                            }
-                            else
-                            {
-                                writer.Write(value.PadRight(columnWidths[i] + 2));
-                            }
-                        }
-
-                        writer.WriteLine();
-                    }
-                }
-                else
-                {
-                    throw new ArgumentNullException(nameof(writer));
-                }
-            }
-            else
+#pragma warning disable CA1510 // Use ArgumentNullException throw helper
+            if (collection == null)
             {
                 throw new ArgumentNullException(nameof(collection));
+            }
+#pragma warning restore CA1510 // Use ArgumentNullException throw helper
+
+#pragma warning disable CA1510 // Use ArgumentNullException throw helper
+            if (writer == null)
+            {
+                throw new ArgumentNullException(nameof(writer));
+            }
+#pragma warning restore CA1510 // Use ArgumentNullException throw helper
+
+            if (collection.Count == 0)
+            {
+                throw new ArgumentException("Collection cannot be empty.", nameof(collection));
+            }
+
+            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                                      .Where(p => p.PropertyType.IsPrimitive || p.PropertyType == typeof(string) || p.PropertyType == typeof(decimal))
+                                      .ToArray();
+
+            if (properties.Length == 0)
+            {
+                return;
+            }
+
+            var columnWidths = new int[properties.Length];
+
+            foreach (var item in collection)
+            {
+                for (int i = 0; i < properties.Length; i++)
+                {
+                    var value = properties[i].GetValue(item)?.ToString() ?? string.Empty;
+                    columnWidths[i] = Math.Max(columnWidths[i], value.Length);
+                }
+            }
+
+            // Write header border
+            writer.Write("+");
+            for (int i = 0; i < properties.Length; i++)
+            {
+                var header = properties[i].Name;
+                columnWidths[i] = Math.Max(columnWidths[i], header.Length);
+                writer.Write(new string('-', columnWidths[i] + 2) + "+");
+            }
+
+            writer.WriteLine();
+
+            // Write header
+            writer.Write("|");
+            for (int i = 0; i < properties.Length; i++)
+            {
+                var header = properties[i].Name;
+                writer.Write(" " + header.PadRight(columnWidths[i]) + " |");
+            }
+
+            writer.WriteLine();
+
+            // Write header border
+            writer.Write("+");
+            for (int i = 0; i < properties.Length; i++)
+            {
+                writer.Write(new string('-', columnWidths[i] + 2) + "+");
+            }
+
+            writer.WriteLine();
+
+            // Write rows
+            foreach (var item in collection)
+            {
+                writer.Write("|");
+                for (int i = 0; i < properties.Length; i++)
+                {
+                    var value = properties[i].GetValue(item)?.ToString() ?? string.Empty;
+                    if (properties[i].PropertyType == typeof(int) || properties[i].PropertyType == typeof(double) || properties[i].PropertyType == typeof(float) || properties[i].PropertyType == typeof(decimal) || properties[i].PropertyType == typeof(DateTime))
+                    {
+                        writer.Write(" " + value.PadLeft(columnWidths[i]) + " |");
+                    }
+                    else
+                    {
+                        writer.Write(" " + value.PadRight(columnWidths[i]) + " |");
+                    }
+                }
+
+                writer.WriteLine();
+
+                // Write row border
+                writer.Write("+");
+                for (int i = 0; i < properties.Length; i++)
+                {
+                    writer.Write(new string('-', columnWidths[i] + 2) + "+");
+                }
+
+                writer.WriteLine();
             }
         }
     }
